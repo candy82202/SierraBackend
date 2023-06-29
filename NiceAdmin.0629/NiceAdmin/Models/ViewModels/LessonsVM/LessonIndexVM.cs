dd8bf7781e057cb0b65c5263e6e921cbdd6cece8
JamesHsieh1102 <casear1102@gmail.com>
2023-06-29 23:48:30 +0800

    [V]初步修改Lessons、LessonCategories CRUD版面調整

--- NiceAdmin.0629/NiceAdmin/Models/ViewModels/LessonsVM/LessonIndexVM.cs
+++ NiceAdmin.0629/NiceAdmin/Models/ViewModels/LessonsVM/LessonIndexVM.cs
@@ -1,63 +1,66 @@
-﻿using System;
+﻿using NiceAdmin.Models.EFModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Xml.Linq;
 
-namespace NiceAdmin.Models.ViewModels.CoursesVM
+namespace NiceAdmin.Models.ViewModels.LessonsVM
 {
     public class LessonIndexVM
     {
+        public int LessonId { get; set; }
 
         [Display(Name = "課程分類")]
         [StringLength(50)]
-        public int CourseCategoryName { get; set; }
+        public string LessonCategoryName { get; set; }
 
         [Display(Name = "課程名稱")]
         [StringLength(50)]
-        public string CourseTitle { get; set; }
+        public string LessonTitle { get; set; }
 
         [Display(Name = "課程簡介")]
         [StringLength(50)]
-        public string CourseInfo { get; set; }
+        public string LessonInfo { get; set; }
+
+        public string LessonDetail { get; set; }
 
         [Display(Name = "課程內容")]
-        [StringLength(50)]
-        public string CourseDetail
+        public string LessonDetailText
         {
             get
             {
-                return this.CourseDetail.Length > 10
-                        ? this.CourseDetail.Substring(0, 10) + "..."
-                        : this.CourseDetail;
+                return this.LessonDetail.Length > 10
+                        ? this.LessonDetail.Substring(0, 10) + "..."
+                        : this.LessonDetail;
             }
         }
 
         [Display(Name = "課程作品")]
         [StringLength(50)]
-        public string CourseDessert { get; set; }
+        public string LessonDessert { get; set; }
 
         [Display(Name = "開課時間")]
-        public DateTime CourseTime { get; set; }
+        public DateTime LessonTime { get; set; }
 
         [Display(Name = "上課時數")]
-        public int CourseHours { get; set; }
+        public int LessonHours { get; set; }
 
-        [Display(Name = "報名上限")]
+        [Display(Name = "報名人數上限")]
         public int MaximumCapacity { get; set; }
 
         [Display(Name = "售價")]
         [DisplayFormat(DataFormatString = "{0:#,#}")]
-        public int CoursePrice { get; set; }
+        public int LessonPrice { get; set; }
 
-        public bool CourseStatus { get; set; }
+        public bool LessonStatus { get; set; }
         [Display(Name = "是否上架")]
         public string StatusText
         {
             get
             {
-                return this.CourseStatus == true
+                return this.LessonStatus == true
                     ? "上架中" : "已下架";
             }
         }
@@ -65,7 +68,9 @@ namespace NiceAdmin.Models.ViewModels.CoursesVM
         [Display(Name = "發布日期")]
         public DateTime CreateTime { get; set; }
 
-
+        [Display(Name = "教師姓名")]
+        public string TeacherName { get; set; }
+    
     }
 
 
