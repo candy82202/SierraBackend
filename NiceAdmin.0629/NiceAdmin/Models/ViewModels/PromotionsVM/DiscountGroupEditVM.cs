088f209da159fa5adc4451bc97600c4e530ff4b5
Paddy Huang <paddy155777@gmail.com>
2023-06-30 10:39:42 +0800

    vm家回來

---
+++ NiceAdmin.0629/NiceAdmin/Models/ViewModels/PromotionsVM/DiscountGroupEditVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace NiceAdmin.Models.ViewModels
+{
+	public class DiscountGroupEditVM
+	{
+		public int DiscountGroupId { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[Display(Name = "群組名稱")]
+		public string DiscountGroupName { get; set; }
+
+		public IEnumerable<DessertInDiscountGroup> Desserts { get; set; }
+
+		public IEnumerable<DiscountGroupItemVM> DiscountGroupItems { get; set; }
+
+	}
+	public class DessertInDiscountGroup
+	{
+		public int? DessertId { get; set; }
+
+		public string DessertName { get; set; }
+
+		public string CategoryName { get; set; }
+
+		public int? UnitPrice { get; set; }
+	}
+	public class DiscountGroupItemVM
+	{
+		public int DiscountGroupItemId { get; set; }
+
+		public int DiscountGroupId { get; set; }
+
+		public int? DessertId { get; set; }
+
+		public DessertInDiscountGroup Dessert { get; set; }
+	}
+
+}
--- NiceAdmin.0629/NiceAdmin/Models/ViewModels/PromotionsVM/DiscountGroupExts.cs
+++ NiceAdmin.0629/NiceAdmin/Models/ViewModels/PromotionsVM/DiscountGroupExts.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using NiceAdmin.Models.EFModels;
+using NiceAdmin.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace NiceAdmin.Models.ViewModels.PromotionsVM
+namespace NiceAdmin.Models.Exts
 {
-	public class DiscountGroupExts
+	public static class DiscountGroupExts
 	{
+		public static DessertInDiscountGroup ToDessertInDiscountGroup(this Dessert entity)
+		{
+			return new DessertInDiscountGroup()
+			{
+				DessertId = entity.DessertId,
+				DessertName = entity.DessertName,
+				CategoryName = entity.Category.CategoryName,
+				UnitPrice = entity.UnitPrice,
+			};
+		}
 	}
 }
